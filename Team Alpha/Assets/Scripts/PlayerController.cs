4c3fabf53527910470c64167ceb12080e394f4c4
patto1234 <160696095+patto1234@users.noreply.github.com>
2025-06-11 16:49:40 +1000

    top down movement

--- Team Alpha/Assets/Scripts/PlayerController.cs
+++ Team Alpha/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,40 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
 
-    // Start is called before the first frame update
+    public LayerMask whatStopsMovement;
+
+    
     void Start()
     {
         movePoint.parent = null;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(0.5f,0,0.5f) + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement).Length <= 0)
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+                
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (Physics.OverlapSphere(movePoint.position + new Vector3(0.5f, 0, 0.5f) + new Vector3(0f, 0f, Input.GetAxisRaw("Vertical")), .2f, whatStopsMovement).Length <= 0)
+                {
+                    movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+                }
+
         
+            }
+        }
+
     }
 }
