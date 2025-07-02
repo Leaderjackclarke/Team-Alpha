b04f81419aab315ffa30a0c5f573b5cdae861c47
Leaderjackclarke <132954406+Leaderjackclarke@users.noreply.github.com>
2025-07-02 15:51:56 +1000

    update from main

---
+++ Team Alpha/Assets/scripts/pushable object.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableObject : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Vector3 movePoint;
+    public Collision collision;
+
+    public LayerMask whatStopsMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint, moveSpeed * Time.deltaTime);
+    }
+
+    public void PushObject(Vector3 newMovePosition)
+    {
+        if (Vector3.Distance(transform.position, movePoint) <= .05f)
+        {
+            if (Mathf.Abs(newMovePosition.x) == 1f || Mathf.Abs(newMovePosition.z) == 1)
+            {
+                if (Physics.OverlapSphere(movePoint + newMovePosition, .2f, whatStopsMovement).Length <= 0)
+                {
+                    movePoint += newMovePosition;
+                }
+            }
+        }
+    }
+
+
+}
+    
+        
+ 
