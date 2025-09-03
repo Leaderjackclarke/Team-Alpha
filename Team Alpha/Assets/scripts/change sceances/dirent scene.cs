96004bc55bd665781d6d3346339ab22b89c28f06
Leaderjackclarke <132954406+Leaderjackclarke@users.noreply.github.com>
2025-09-03 16:41:20 +1000

    add this

---
+++ Team Alpha/Assets/scripts/change sceances/dirent scene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class direntscene : MonoBehaviour
+{
+
+
+    public void Loadnewlevel()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+}
